c61e3394aa2407b8fd41f3c62a2c795f0992d2c5
Daksin World <onosov20071@gmail.com>
2022-08-24 20:43:00 +0200

    changed main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  })
+  app.enableCors()
   require('dotenv').config({ path: '.env' })
   await app.listen(process.env.PORT || 3000);
 }

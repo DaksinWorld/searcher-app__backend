47485f026dc4a7a4890e370c5d56c8b6b424efcf
Daksin World <onosov20071@gmail.com>
2022-08-24 20:50:05 +0200

    new commit

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
-  app.enableCors()
+  app.enableCors({
+    allowedHeaders:"*",
+    origin: "*"
+  });
   require('dotenv').config({ path: '.env' })
   await app.listen(process.env.PORT || 3000);
 }
